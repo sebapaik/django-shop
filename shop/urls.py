184825cd8a883d0f3b46fe09795aacbc57469c6d
sebapaik <31723260+sebapaik@users.noreply.github.com>
2018-09-10 02:48:51 -0300

    a

--- shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProductListView, ProductDetailView
-from . import views
-
-urlpatterns = [
-    #path('', views.index, name = 'shop-index'),
-    path('', ProductListView.as_view(), name = 'shop-index'),
-    path('<int:pk>/', ProductDetailView.as_view(), name = 'shop-product'),
-]
